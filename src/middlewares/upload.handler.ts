241b69a7822f0e8059867f73361d22b75ed4392a
Claudio Narvaez <claudionarvaez61@gmail.com>
2025-07-31 05:22:25 -0400

    Solve upload file bug

--- src/middlewares/upload.handler.ts
+++ src/middlewares/upload.handler.ts
@@ -2,6 +2,4 @@ import multer from 'multer';
 import { localStorage } from '../config/local.storage';
 
 const storage = localStorage;
-const uploadFile = multer({ storage });
-
-export default uploadFile;
+export const uploadFile = multer({ storage });;
